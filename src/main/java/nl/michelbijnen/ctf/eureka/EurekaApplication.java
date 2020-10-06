1866544ebca2bb956d7acc6b13a7164fb33cf130
Michel Bijnen <michelbijnenmichelbijnen@gmail.com>
2020-10-06 15:14:54 +0200

    feat: initial commit

---
+++ src/main/java/nl/michelbijnen/ctf/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package nl.michelbijnen.ctf.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/michelbijnen/ctf/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.michelbijnen.ctf.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
